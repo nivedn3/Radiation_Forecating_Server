9f0e8e8daf9d3ed5c665f446daadd65af6b7f537
Nived <nivedn3@gmail.com>
2017-08-21 07:55:21 +0530

    test

--- Radiation/archive_scrapper/archive_scrapper/spiders/forspef_archive.py
+++ Radiation/archive_scrapper/archive_scrapper/spiders/forspef_archive.py
@@ -11,13 +11,13 @@ class ForspefArchiveScrapper(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        
+
         tables = response.css("table")
         SEPprobsD = tables[3].css("tr")[2:]
-        
+
         dates = []
-        probs =  []
-        
+        probs = []
+
         count = 0
         for j in tables:
             count = count + 1
@@ -27,10 +27,9 @@ class ForspefArchiveScrapper(scrapy.Spider):
                 data = i.css('td')
                 date = data[0].css("td::text").extract_first()
                 prob = data[-1].css("td::text").extract_first()
-                if float(prob)>0.25:
+                if float(prob) > 0.25:
                     dates.append(date)
                     probs.append(prob)
         print dates
         print "\n"
         print probs
-
